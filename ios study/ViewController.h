96cf82ab678083521417824aa9e7cd915e1700c2
현진 이 <werwe@hyeonjin-ui-MacBook-Pro.local>
2014-11-10 23:51:57 +0900

    Initial Commit

---
+++ ios study/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios study
+//
+//  Created by 현진 이 on 2014. 11. 10..
+//  Copyright (c) 2014년 werwe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios study/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ios study
+//
+//  Created by 현진 이 on 2014. 11. 10..
+//  Copyright (c) 2014년 werwe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
